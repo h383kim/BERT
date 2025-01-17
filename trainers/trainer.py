b2aeea1d3e6c70f6d6a8a7026494b0cb54ebbd17
김현태 <kht1310@naver.com>
2025-01-16 23:28:01 -0500

    Trainer

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -46,4 +46,36 @@ def epoch_train(model, train_dataloader, loss_fn, optimizer, scheduler=None):
     # Compute the average loss across all batches
     train_loss /= len(train_dataloader)
     return train_loss
-    
+
+
+"""
+Evaluate the model on a validation dataset (without parameter updates).
+Args:
+    model (nn.Module): The PyTorch model to be evaluated.
+    val_dataloader (DataLoader): Dataloader providing the validation data.
+                                 Each batch is expected to be a list of strings.
+    loss_fn (nn.Module): The loss function used (e.g., nn.CrossEntropyLoss()).
+Returns:
+    float: The average validation loss over the entire dataset.
+"""
+def evaluate(model, val_dataloader, loss_fn):
+    model.eval()
+    eval_loss = 0.0
+
+    for X, y_MTP, y_NSP, seg_ids in tqdm(val_dataloader, desc="Validation"):
+        # Moving data to cpu or gpu
+        X, y_MTP, y_NSP, seg_ids = X.to(DEVICE), y_MTP.to(DEVICE), y_NSP.to(DEVICE), seg_ids.to(DEVICE)
+        
+        # Feed Forward
+        NSP_logits, MTP_logits, _ = model(X, seg_ids)
+        
+        # Calculate Losses
+        loss_NSP = loss_fn(NSP_logits, y_NSP)
+        loss_MTP = loss_fn(MTP_logits.permute(0, 2, 1), y_MTP)
+        loss = loss_NSP + loss_MTP
+        batch_loss = loss.item() * X.shape[0]
+        val_loss += batch_loss
+
+    # Compute the average loss across all batches
+    val_loss /= len(val_dataloader)
+    return val_loss
