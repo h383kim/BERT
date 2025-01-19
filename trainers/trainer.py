13335512dfb676baeb75594f917bffef81b6bc99
김현태 <kht1310@naver.com>
2025-01-18 23:30:12 -0500

    Trainer

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -79,3 +79,57 @@ def evaluate(model, val_dataloader, loss_fn):
     # Compute the average loss across all batches
     val_loss /= len(val_dataloader)
     return val_loss
+
+
+
+"""
+Main training loop that orchestrates the training and evaluation phases,
+and tracks the best model weights based on validation loss.
+Args:
+    model (nn.Module): The PyTorch model to be trained.
+    train_dataloader (DataLoader): Dataloader providing the training data.
+    val_dataloader (DataLoader): Dataloader providing the validation data.
+    loss_fn (nn.Module): Loss function to be used during training/evaluation.
+    optimizer (torch.optim.Optimizer): Optimizer used for model updates.
+    scheduler (torch.optim.Optimizer): Scheduler used for lr updates.
+    num_epochs (int, optional): Number of epochs to train. Defaults to 1.
+Returns:
+    nn.Module: The trained model with the best weights (lowest validation loss).
+"""
+def train(model, train_dataloader, val_dataloader, loss_fn, optimizer, scheduler, num_epochs=1):
+    # Track the best validation loss and corresponding model weights
+    best_loss = float('inf')  # or some large constant
+    best_wts = copy.deepcopy(model.state_dict())
+    
+    # For logging losses across epochs
+    loss_dict = {'train_loss': [],
+                 'val_loss': []}
+
+    # Loop over the desired number of epochs
+    for epoch in range(1, num_epochs + 1):
+        start = time()
+        # Feed forward / backprop on train_dataloader
+        train_loss = epoch_train(model, train_dataloader, loss_fn, optimizer, scheduler)
+        # Feed forward on val_dataloader
+        val_loss = evaluate(model, val_dataloader, loss_fn)
+
+        # Storing epoch histories
+        loss_dict['train_loss'].append(train_loss)
+        loss_dict['val_loss'].append(val_loss)
+
+        # Update model depending on its peformance on validation data
+        if val_loss < best_loss:
+            best_loss = val_loss
+            best_wts = copy.deepcopy(model.state_dict())
+
+        # Print epoch summary
+        end = time()
+        time_elapsed = end - start
+        print(f"------------ epoch {epoch} ------------")
+        print(f"Train loss: {train_loss:.4f}")
+        print(f"Validation loss: {val_loss:.4f}")
+        print(f"Time taken: {time_elapsed / 60:.0f}min {time_elapsed % 60:.0f}s")
+
+    # Load the best weights (lowest validation loss) into the model
+    model.load_state_dict(best_wts)
+    return model
